ce414000bbfefcc44ddaaa03c5d5114491a85705
Gustavo Lobo <gustavolobo90@gmail.com>
2016-10-27 22:25:33 +0100

    Setup initial structure.

--- app/commands_executor.rb
+++ app/commands_executor.rb
@@ -3,7 +3,14 @@ class CommandsExecutor
   UNRECOGNISED_COMMAND = -> (_) { puts 'unrecognised command :(' }
 
   def initialize
-    @mappings = {}
+    @mappings = {
+      'I' => -> (args) { create_bitmap(args) },
+      'C' => -> (_)    { clear_bitmap },
+      'L' => -> (args) { color_pixel(args) },
+      'V' => -> (args) { draw_vertical_line(args) },
+      'H' => -> (args) { draw_horizontal_line(args) },
+      'S' => -> (_)    { show_bitmap }
+    }
   end
 
   def call(input)
@@ -13,9 +20,28 @@ class CommandsExecutor
 
   private
 
+  def create_bitmap(args)
+  end
+
+  def clear_bitmap
+  end
+
+  def color_pixel(args)
+  end
+
+  def draw_vertical_line(args)
+  end
+
+  def draw_horizontal_line(args)
+  end
+
+  def show_bitmap
+  end
+
   def executor_for(command)
     mappings[command] || UNRECOGNISED_COMMAND
   end
 
+  attr_accessor :bitmap
   attr_reader :mappings
 end
--- spec/commands_executor_spec.rb
+++ spec/commands_executor_spec.rb
@@ -3,4 +3,21 @@ require_relative '../app/commands_executor'
 
 describe CommandsExecutor do
   specify { expect { subject.call('bla') }.to output("unrecognised command :(\n").to_stdout }
+
+  describe '#call' do
+    {
+      'I M N'       => :create_bitmap,
+      'C'           => :clear_bitmap,
+      'L X Y C'     => :color_pixel,
+      'V X Y1 Y2 C' => :draw_vertical_line,
+      'H X1 X2 Y C' => :draw_horizontal_line,
+      'S'           => :show_bitmap
+    }.each do |input, expected_method_call|
+      it 'calls the right method' do
+        allow(subject).to receive(expected_method_call)
+        subject.call(input)
+        expect(subject).to have_received(expected_method_call)
+      end
+    end
+  end
 end
