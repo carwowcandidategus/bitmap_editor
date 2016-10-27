50536155f2b0889ac5bff8e6976ec959a354f77d
Gustavo Lobo <gustavolobo90@gmail.com>
2016-10-27 21:28:45 +0100

    Make CommandsExecutor a bit more scalable.

--- app/commands_executor.rb
+++ app/commands_executor.rb
@@ -1,6 +1,21 @@
 # frozen_string_literal: true
 class CommandsExecutor
-  def call(input)
-    puts 'unrecognised command :('
+  UNRECOGNISED_COMMAND = -> (_) { puts 'unrecognised command :(' }
+
+  def initialize
+    @mappings = {}
   end
+
+  def call(input)
+    command, arguments = input.split(' ', 2)
+    executor_for(command).call(arguments)
+  end
+
+  private
+
+  def executor_for(command)
+    mappings[command] || UNRECOGNISED_COMMAND
+  end
+
+  attr_reader :mappings
 end
